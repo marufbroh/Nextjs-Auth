ad1f3d5fca639be851ae46acc227fcf21fdc666f
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-20 22:26:06 +0600

    basic google configuration done

--- crash-course/app/api/auth/[...nextauth]/route.js
+++ crash-course/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,2 @@
-import Google from "next-auth/providers/google"
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    Google({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-})
+import { handlers } from "@/auth"
+export const { GET, POST } = handlers
